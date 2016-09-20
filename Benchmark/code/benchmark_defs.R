8b6135b0b4c712cb94da764ebc3b3c25dbbf4c9d
PhilippPro <philipp_probst@gmx.de>
2016-09-20 10:38:59 +0200

    changed it also in the benchmark part

--- Benchmark/code/benchmark_defs.R
+++ Benchmark/code/benchmark_defs.R
@@ -382,7 +382,7 @@ makeRLearner.classif.brf.conv = function() {
 
 trainLearner.classif.brf.conv = function(.learner, .task, .subset, .weights = NULL, ...) {
   f = getTaskTargetNames(.task)
-  data = getTaskData(.task)
+  data = getTaskData(.task, subset = .subset)
   TY = data[, f]
   TX = data[,colnames(data) != f]
   brf.conv(TY = TY, TX = TX, ...)
@@ -395,4 +395,37 @@ predictLearner.classif.brf.conv = function(.learner, .model, .newdata, ...) {
     p = brf.predict(.model$learner.model, data = .newdata, prob = TRUE)
   }
   return(p)
+}
+
+makeRLearner.regr.brf.conv = function() {
+  makeRLearnerRegr(
+    cl = "regr.brf.conv",
+    package = "MASS",
+    par.set = makeParamSet(
+      makeNumericLearnerParam(id = "forest.size", lower = 20, default = 300),
+      #makeNumericLearnerParam(id = "init.weights", default = FALSE),
+      makeNumericLearnerParam(id = "weight.treshold", default = 20, lower = 20),
+      makeLogicalLearnerParam(id = "leaf.weights", default = FALSE, tunable = FALSE),
+      makeLogicalLearnerParam(id = "converge", default = TRUE, tunable = FALSE),
+      makeLogicalLearnerParam(id = "sample.weights", default = TRUE, tunable = FALSE),
+      makeNumericLearnerParam(id = "smoothness", default = 30, lower = 20),
+      makeNumericLearnerParam(id = "conv.treshold", default = 0.01, upper = 0.3)
+    ),
+    properties = c("numerics", "factors"),
+    name = "Boosted Random Forest",
+    short.name = "brf.conv",
+    note = ""
+  )
+}
+
+trainLearner.regr.brf.conv = function(.learner, .task, .subset, .weights = NULL, ...) {
+  f = getTaskTargetNames(.task)
+  data = getTaskData(.task, subset = .subset)
+  TY = data[, f]
+  TX = data[,colnames(data) != f]
+  brf.conv(TY = TY, TX = TX, ...)
+}
+
+predictLearner.regr.brf.conv = function(.learner, .model, .newdata, ...) {
+  brf.predict(.model$learner.model, data = .newdata, prob = FALSE)
 }
