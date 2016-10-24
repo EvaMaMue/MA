405da8fa9de6d397b5eb8dac3631d26364f35b2a
PhilippPro <philipp_probst@gmx.de>
2016-10-24 14:11:01 +0200

    update the benchmark code again

--- Benchmark/code/benchmark.R
+++ Benchmark/code/benchmark.R
@@ -30,10 +30,10 @@ addAlgorithm("eval", fun = function(job, data, instance,  ...) {
   type = getTaskType(task)
   # set here better defaults for each package?
   if(type == "classif") {
-    learners = list(makeLearner("classif.rfsrc", par.vals = list(ntree = 5000), predict.type = "prob"), makeLearner("classif.brf.conv", predict.type = "prob"))
+    learners = list(makeLearner("classif.randomForestSRC", par.vals = list(ntree = 5000), predict.type = "prob"), makeLearner("classif.brf.conv", predict.type = "prob"))
     }
   if(type == "regr"){
-    learners = list(makeLearner("regr.rfsrc", par.vals = list(ntree = 5000)), makeLearner("regr.brf.conv"))
+    learners = list(makeLearner("regr.randomForestSRC", par.vals = list(ntree = 5000)), makeLearner("regr.brf.conv"))
   }
   measures = MEASURES(type)
   rdesc = makeResampleDesc("RepCV", folds = 2, reps = 2, stratify = FALSE)
@@ -50,15 +50,15 @@ summarizeExperiments()
 ids = chunkIds(findNotDone(), chunk.size = 5)
 submitJobs(ids)
 # ranger braucht ca. 3 h für regression + classification
-submitJobs(2)
+submitJobs(5)
 
 #submitJobs(ids, resources = list(chunk.ncpus = 9))
 getStatus()
 getErrorMessages()
 
-res_classif_load = reduceResultsList(ids = 2, fun = function(r) as.list(r), reg = regis)
+res_classif_load = reduceResultsList(ids = 5, fun = function(r) as.list(r), reg = regis)
 
-
+regis = loadRegistry("benchmark-rf-brf")
 # zu Debugzwecken
 #lrn.id = "ranger"
 #par.vals = as.list(ades[1,-1])
