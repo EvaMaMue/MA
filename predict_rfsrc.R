2425c6082c8c5586c768338c00b1aea115d3998f
EvaMaMue <evamariam1102@gmx.de>
2016-11-13 12:38:18 +0100

    Update predict_rfsrc.R
    nur für Klassifikation
    Fehler ausgebessert für leaf.weights Option

--- predict_rfsrc.R
+++ predict_rfsrc.R
@@ -5,32 +5,23 @@ predict.brf <- function(object, data, prob = FALSE){
   if(object$type == "regression" && prob){
     stop("probability only for classification possible")
   }
+  
   predictions.matrix <- vector(mode="numeric")
-  if(object$leaf.weights){
-    vote.matrix <- matrix(data = 0, nrow = dim(data)[1], ncol = object$num.levels)
-  }
-  
-  
-
   
   if (object$type == "classification") {
     for(t in 1:(object$num.trees)){
-      predictions.matrix <- cbind(predictions.matrix, predict(object$forest[[t]], newdata = data)$class)
+      if(!object$leaf.weights){
+        predictions.matrix <- cbind(predictions.matrix, predict(object$forest[[t]], data)$class)
+      }
       if(object$leaf.weights){
-        for(i in 1:object$num.levels){
-          vote.matrix[which(predictions.matrix[,t]==i),i] <- vote.matrix[which(predictions.matrix[,t]==i),i] + object$weights[which(predictions.matrix[,t]==i),t]
-        }
+        predNode <- predict(object$forest[[t]], data, membership = T)$membership # in welchem Endknoten landet Beobachtung i
+        predClass <- apply(predNode,2, function(x) as.integer(object$majClassNodesForest[[t]][x]))  # welcher Klasse entspricht dieser Knoten in diesem Baum
+        predictions.matrix <- cbind(predictions.matrix, predClass)
       }
     }
     if(!prob){
-      if(!object$leaf.weights){
-        predictions <- object$lev.names[as.numeric(apply(predictions.matrix, 1, function(x) names(which.max(table(x)))))]
-        predictions <- factor(predictions, levels = object$lev.names)
-      } 
-      if(object$leaf.weights){
-        predictions <- object$lev.names[as.numeric(apply(vote.matrix, 1, function(x) which.max(x)))]
-        predictions <- factor(predictions, levels = object$lev.names)
-      }
+      predictions <- object$lev.names[as.numeric(apply(predictions.matrix, 1, function(x) names(which.max(table(x)))))]
+      predictions <- factor(predictions, levels = object$lev.names)
       
       return(predictions)
     }
@@ -40,16 +31,12 @@ predict.brf <- function(object, data, prob = FALSE){
       for(i in 1:object$num.levels){
         predictions.prob <- cbind(predictions.prob, apply(predictions.matrix,1, function(x) length(which(x==i))))
       }
-      predictions.prob <- data.frame(predictions.prob/(object$num.trees))
+      predictions.prob <- predictions.prob/(object$num.trees)
       colnames(predictions.prob) <- object$lev.names
       return(as.matrix(predictions.prob))
     }
   }
-  
-  if (object$type == "regression") {
-    for(t in 1:(object$num.trees)){
-      predictions.matrix <- cbind(predictions.matrix, predict(object$forest[[t]], newdata = data)$predicted)
-    }
-    return(apply(predictions.matrix, 1, mean))
-  }
 }
+
+
+
